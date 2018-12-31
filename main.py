ee047bf696e53f5de49a1377967e4e843690c773
Jddanan <jddanan@gmail.com>
2018-12-31 16:52:53 +0200

    test on heroku

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ from backend.page_handlers import pageHandler
 app.merge(staticHandler)
 app.merge(pageHandler)
 
-# run(app, host='0.0.0.0', port=os.environ.get('PORT', 5000))
-if __name__ == "__main__":
-    run(app, host='localhost', port=7000)
+run(app, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+#if __name__ == "__main__":
+#    run(app, host='localhost', port=7000)
